9746454ad9876653fbe07068a95f5ea804cc58af
Larry Chen <102985198+LarryChenCode@users.noreply.github.com>
2024-04-17 12:50:12 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,10 @@
-# This is a comment. It is ignored by the Python interpreter.
-# Comments are used to explain the code and make it easier to read.
+# Ask the user for their name, age, and favorite color
+firstName = input("What's your first name?\n")
+lastName = input("What's your last name?\n")
+age = input("How old are you?\n")
+color = input("Last question, I promise!!!\nWhat is your favorite color?\n") 
 
-name = input("What's your name? ")
-age = input("How old are you? ")
-
-# The input() function is used to prompt the user for input.
-# Whatever the user enters is returned as a string, and is stored in the variables "name" and "age".
-
-print("Hello, " + name + "!")
+# Print out the answer with a greeting
+print("\nHello, " + firstName + " " + lastName + "!")
 print("You are " + age + " years old.")
-
-# The print() function is used to generate output. It can be used to print strings, variables, and more.
-# The "+" operator is used to concatenate (join) strings together.
+print("You like the color " + color + ".")
